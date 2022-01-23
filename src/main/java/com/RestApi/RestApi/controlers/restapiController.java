53e2ee5b9da4a19cffeb123785ec1328ec22aa8d
unknown <akshayHVshinde04@gmail.com>
2022-01-23 16:14:34 +0530

    Post Request Method

--- src/main/java/com/RestApi/RestApi/controlers/restapiController.java
+++ src/main/java/com/RestApi/RestApi/controlers/restapiController.java
@@ -1,12 +1,15 @@
 package com.RestApi.RestApi.controlers;
 
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.RestApi.RestApi.model.User;
 
 @RestController
 @RequestMapping("/restapi")
@@ -26,4 +29,8 @@ public class restapiController {
 		return "Hello " + name + " Join the Meeting ";
 	}
 	
+	@PostMapping ("/post")
+	public String helloMsg(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName();
+	}
 }
