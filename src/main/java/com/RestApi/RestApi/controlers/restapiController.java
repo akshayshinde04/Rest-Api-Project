b7c6997cb911d85d9a16bb870366d7687240d293
unknown <akshayHVshinde04@gmail.com>
2022-01-23 11:01:02 +0530

    Hello Name

--- src/main/java/com/RestApi/RestApi/controlers/restapiController.java
+++ src/main/java/com/RestApi/RestApi/controlers/restapiController.java
@@ -1,6 +1,9 @@
 package com.RestApi.RestApi.controlers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,11 @@ public class restapiController {
 	public String helloMsg() {
 		return "Hello from Bridgelabz";
 	}
+	
+	@RequestMapping (value = {"/query"}, method = RequestMethod.GET)
+	public String helloMsg(@RequestParam (value = "name") String name) {
+		return "Hello" + name + "!";
+	}
+	
+	
 }
